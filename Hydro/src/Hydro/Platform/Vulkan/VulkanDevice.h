40773cc562c9c12cd3a0154f069ffc3128bb57d6
Joey <joelnl74@gmail.com>
2022-07-23 14:27:23 +0200

    Create vulkan physical device class

--- Hydro/src/Hydro/Platform/Vulkan/VulkanDevice.h
+++ Hydro/src/Hydro/Platform/Vulkan/VulkanDevice.h
@@ -1,35 +1,9 @@
 #pragma once
 #include <vulkan/vulkan.h>
+#include "Hydro/Platform//Vulkan/VulkanPhysicalDevice.h"
 
 namespace Hydro
 {
-	struct QueueFamilyIndices
-	{
-		int32_t Graphics = -1;
-	};
-
-	class VulkanPhysicalDevice
-	{
-	public:
-		VulkanPhysicalDevice(VkInstance instance);
-		VkPhysicalDevice GetVulkanPhysicalDevice() const { return m_PhysicalDevice; }
-		QueueFamilyIndices GetQueueFamilyIndices() { return m_QueueFamilyIndices; }
-
-	private:
-		bool IsDeviceSuitable(VkPhysicalDevice device);
-		QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
-
-	private:
-		VkPhysicalDevice m_PhysicalDevice = nullptr;
-		VkPhysicalDeviceProperties m_Properties;
-		VkPhysicalDeviceFeatures m_Features;
-		VkPhysicalDeviceMemoryProperties m_MemoryProperties;
-
-		QueueFamilyIndices m_QueueFamilyIndices;
-		
-		uint32_t deviceCount = 0;
-	};
-
 	class VulkanDevice
 	{
 	public:
---
+++ Hydro/src/Hydro/Platform/Vulkan/VulkanPhysicalDevice.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vulkan/vulkan.h>
+
+namespace Hydro
+{
+	struct QueueFamilyIndices
+	{
+		int32_t Graphics = -1;
+	};
+
+	class VulkanPhysicalDevice
+	{
+	public:
+		VulkanPhysicalDevice(VkInstance instance);
+		VkPhysicalDevice GetVulkanPhysicalDevice() const { return m_PhysicalDevice; }
+		QueueFamilyIndices GetQueueFamilyIndices() { return m_QueueFamilyIndices; }
+
+	private:
+		bool IsDeviceSuitable(VkPhysicalDevice device);
+		QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+
+	private:
+		VkPhysicalDevice m_PhysicalDevice = nullptr;
+		VkPhysicalDeviceProperties m_Properties;
+		VkPhysicalDeviceFeatures m_Features;
+		VkPhysicalDeviceMemoryProperties m_MemoryProperties;
+
+		QueueFamilyIndices m_QueueFamilyIndices;
+
+		uint32_t deviceCount = 0;
+	};
+}
+
+
